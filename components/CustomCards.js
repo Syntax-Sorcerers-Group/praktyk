881ff8b57d0e980f66765756d9e8ba94b0438566
Mu'aaz Bassa <muaazbassa@gmail.com>
2023-08-25 23:24:17 +0200

    PRAK-4 Added Card Components

---
+++ components/CustomCards.js
@@ -0,0 +1,52 @@
+// import * as React from 'react';
+// import { Avatar, Button, Card, Text } from 'react-native-paper';
+
+// const LeftContent = props => <Avatar.Icon {...props} icon="folder" />
+
+// const CustomCard = ({ title, imageUrl }) => (
+//   <Card>
+//     <Card.Title title={title} left={LeftContent} />
+//     <Card.Cover source={{ uri: imageUrl }} />
+//     <Card.Actions>
+//       <Button>Cancel</Button>
+//       <Button>Ok</Button>
+//     </Card.Actions>
+//   </Card>
+// );
+
+// export default CustomCard;
+
+import * as React from "react";
+import { Avatar, Button, Card, Text } from "react-native-paper";
+import { View, StyleSheet } from "react-native";
+
+const CustomCard = ({ title, imageUrl }) => (
+  <View style={styles.cardContainer}>
+    <Card style={styles.card}>
+    <Card.Title
+        title={<Text style={styles.title}>{title}</Text>} // Apply styling to the title
+        titleStyle={styles.title} // Ensure the title style is applied
+      />
+      <Card.Cover source={{ uri: imageUrl }} />
+      <Card.Actions>
+          <Button>Cancel</Button>
+          <Button>Ok</Button>
+        </Card.Actions>
+    </Card>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  cardContainer: {
+    width: "100%",
+    paddingHorizontal: 16,
+  },
+  card: {
+    marginBottom: 16,
+  },
+  title: {
+    textAlign: "center",
+  },
+});
+
+export default CustomCard;
--- pages/GrammarScreen.js
+++ pages/GrammarScreen.js
@@ -1,14 +1,31 @@
 import React from "react";
-import { View, Text, StyleSheet, Button} from "react-native";
-import { useNavigation } from '@react-navigation/native';
+import { View, Text, StyleSheet, Button, ScrollView } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import CustomCard from "../components/CustomCards";
 
-export default function HomeScreen({ navigation }) {
+export default function GrammarScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text style={{ fontSize: 26, fontWeight: 'bold' }}>
-            GRAMMAR SCREEN
-        </Text>
-        <Button title="Grammar Category" onPress={() => {navigation.navigate('Tabless', { screen: 'GrammarCategory' })}} />
-    </View>
+    <ScrollView >
+      
+      <Button
+        title="Grammar Category"
+        onPress={() => {
+          navigation.navigate("Tabless", { screen: "GrammarCategory" });
+        }}
+      />
+
+      <CustomCard title="Custom Card 1" imageUrl="https://picsum.photos/700" />
+      <CustomCard title="Custom Card 2" imageUrl="https://picsum.photos/800" />
+      <CustomCard title="Custom Card 3" imageUrl="https://picsum.photos/900" />
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 20, // Add some padding to space out the content
+  },
+});
