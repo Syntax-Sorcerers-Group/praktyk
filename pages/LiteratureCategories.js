8f718c83b60d1dceb4bebbb2c66dfebe27f2c1cb
Mu'aaz Bassa <muaazbassa@gmail.com>
2023-11-16 21:19:07 +0200

    Update LiteratureCategories.js

--- pages/LiteratureCategories.js
+++ pages/LiteratureCategories.js
@@ -3,6 +3,12 @@ import { View, Text, StyleSheet, Button } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 export default function LiteratureCategories({ navigation }) {
+  // Retrieve the selectedGrade parameter from the route
+  // const selectedGrade = route.params?.selectedGrade || "Not Selected";
+  const selectedGrade = 12;
+
+  const catergoryField = "set_literature";
+
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text style={{ fontSize: 26, fontWeight: "bold" }}>
@@ -11,7 +17,10 @@ export default function LiteratureCategories({ navigation }) {
       <Button
         title="Literature Learning"
         onPress={() => {
-          navigation.navigate("Literature Learning");
+          navigation.navigate("Literature Learning", {
+            selectedGrade,
+            catergoryField,
+          }); // Pass selectedGrade as a parameter
         }}
       />
     </View>
