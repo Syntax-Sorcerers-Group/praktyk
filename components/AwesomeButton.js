fb60038b3297c943991cbf1228d250694336bd39
Mu'aaz Bassa <muaazbassa@gmail.com>
2023-11-16 20:33:50 +0200

    Changed the colour of the Buttons

--- components/AwesomeButton.js
+++ components/AwesomeButton.js
@@ -2,7 +2,18 @@ import * as React from "react";
 import AwesomeButton from "react-native-really-awesome-button";
 
 const AwesomeButtonComponent = (props) => {
-  const { displayText, icon, mode, onPress, loadingState, isDisabled,width,activeOpacity,textFontFamily,textSize } = props;
+  const {
+    displayText,
+    icon,
+    mode,
+    onPress,
+    loadingState,
+    isDisabled,
+    width,
+    activeOpacity,
+    textFontFamily,
+    textSize,
+  } = props;
   return (
     <AwesomeButton
       icon={icon}
@@ -13,11 +24,11 @@ const AwesomeButtonComponent = (props) => {
       width={width}
       borderRadius={30}
       height={40}
-      borderColor= "black"
-      backgroundColor="#dcbfff"
-      backgroundDarker="#a265d5"
+      borderColor="black"
+      backgroundColor="#0191d0"
+      backgroundDarker="#026e9e"
       //backgroundShadow="white"
-      backgroundActive="#d0a2f7"
+      backgroundActive="#00b1ff"
       textColor="white"
       textSize={textSize}
       activeOpacity={activeOpacity}
